141d63f26a6b5c628f45a9e3c0687b2c36b48894
lucasleandro1 <lukkassoliveira2215@gmail.com>
2023-03-31 16:18:37 -0300

    Add crud ready

--- class/index.php
+++ class/index.php
@@ -51,7 +51,10 @@
       break;
       case "salvar":
         include("salvar-usuario.php");
-        break;
+      break;
+      case "editar":
+        include("editar-usuario.php");
+      break;
       default:
       print"<h1>Bem vindos</h1>";
     }
--- class/salvar-usuario.php
+++ class/salvar-usuario.php
@@ -25,7 +25,38 @@
             }
             break;
         case'editar':
+            $nome= $_POST["nome"];
+            $email= $_POST["email"];
+            $senha= md5($_POST["senha"]);
+            $data_nasc= $_POST["data_nasc"];
+            $sql = "UPDATE usuarios SET nome ='{$nome}',
+            email='{$email}',
+            senha='{$senha}',
+            data_nasc='{$data_nasc}'
+            WHERE 
+                id=".$_REQUEST["id"];
+            $res = $con->query($sql);
+            if ($res == true) {
+                print "<script>alert('Editado com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else {
+                print "<script>alert('Não foi prossivel 
+                editar');</script>";
+                print "<script>location.href=
+                '?page=listar';</script>";
+            }
             break;
         case'excluir':
+            $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+            $res = $con->query($sql);
+            if ($res == true) {
+                print "<script>alert('Excluido com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else {
+                print "<script>alert('Não foi prossivel 
+                excluir');</script>";
+                print "<script>location.href=
+                '?page=listar';</script>";
+            }
             break;
     }
